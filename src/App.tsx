17272d643588f64c1bd076d73baa9e9bf58f03ce
KygenHack <148578932+KygenHack@users.noreply.github.com>
2025-01-13 13:02:02 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1102,6 +1102,7 @@ function App() {
         </div>
       </div>
 
+
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col p-4 pb-20 space-y-5">
         {activeTab === 'home' && (
